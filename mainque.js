fc382e842cf23d5bf880a8aef376f1ed5e002539
Jyothirmai Vanaparthi <113755812+Jyothirmai-123@users.noreply.github.com>
2023-04-30 13:57:22 +0530

    Update mainque.js

--- mainque.js
+++ mainque.js
@@ -2,7 +2,7 @@ function checkAnswer() {
 	var answer = document.getElementById("answer").value;
 	if (answer.toLowerCase() === "fire") {
 		//document.getElementById("result").innerHTML = "Correct!";
-		window.location.href="Congrats.php";
+		window.location.href="congrats.php";
 	} else {
 		document.getElementById("result").innerHTML = "Incorrect";
 	}
